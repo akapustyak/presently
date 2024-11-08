1a03fa70b551dfaf234d965eb37956db1dcc75a5
akapustyak <kapustiak.anna@student.uzhnu.edu.ua>
2024-11-08 14:55:26 +0200

    twytr

--- presently-front/cypress.config.ts
+++ presently-front/cypress.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'fk38gx',
+  projectId: "fk38gx",
+
   component: {
     viewportWidth: 700,
     viewportHeight: 500,
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
