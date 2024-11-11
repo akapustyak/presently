0a386876319c2b734f57ffa9b74e0c0a079416e9
akapustyak <kapustiak.anna@student.uzhnu.edu.ua>
2024-11-11 20:49:45 +0200

    tests

--- presently-front/src/components/GiftCard.cy.tsx
+++ presently-front/src/components/GiftCard.cy.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import GiftCard from './GiftCard'
+import React from 'react';
+import GiftCard from './GiftCard';
 
 describe('<GiftCard />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<GiftCard />)
-  })
-})
+  const giftProps = {
+    title: 'Example Gift',
+    description: 'This is a description of the gift.',
+    link: 'https://example.com',
+  };
+
+  beforeEach(() => {
+    cy.mount(<GiftCard {...giftProps} />);
+  });
+
+  it('renders the GiftCard with correct title, description, and link', () => {
+    cy.contains(giftProps.title).should('exist');
+    cy.contains(giftProps.description).should('exist');
+    cy.get('a').should('have.attr', 'href', giftProps.link);
+  });
+
+  it('displays a link that opens in a new tab', () => {
+    cy.get('a').should('have.attr', 'target', '_blank');
+  });
+});
